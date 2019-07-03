aa800dc12455d072df6d0756151f47f5dc29d18f
Gayan Kaveenda <gayankaveenda@gmail.com>
2019-07-03 23:18:56 +1000

    added load balancer configurations

--- src/main/java/com/developer/photoappapiusers/photoappapiusers/ui/controller/UsersController.java
+++ src/main/java/com/developer/photoappapiusers/photoappapiusers/ui/controller/UsersController.java
@@ -1,5 +1,7 @@
 package com.developer.photoappapiusers.photoappapiusers.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
-    public  String status() {
-        return "wokring";
+    public String status() {
+        return "UsersController wokring" + environment.getProperty("local" +
+                ".server.port");
     }
 }
